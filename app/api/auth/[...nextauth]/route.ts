29ca27fa9188e7a917e9c1f5068b4305f8bea595
Idris Muhammad <mail4idris@gmail.com>
2025-11-18 08:41:18 +0100

    fix: auth bug

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
 // app/api/auth/[...nextauth]/route.ts
-import { auth, handlers } from "@/lib/auth";
+import {  handlers } from "@/lib/auth";
 // Expose Auth.js handlers to Next.js App Router
 export const { GET, POST } = handlers;
